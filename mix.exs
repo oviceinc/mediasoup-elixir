f8f0319e2af4af7a9ae6d9dfce295bf15100efcd
Masataka Shiraki <mshiraki@ovice.co>
2021-07-10 20:00:00 +0900

    chore: coverage uses lcov
    lcov has better tool support.

--- mix.exs
+++ mix.exs
@@ -10,6 +10,11 @@ defmodule MediasoupElixir.MixProject do
       rustler_crates: rustler_crates(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      test_coverage: [
+        tool: LcovEx,
+        output: "coverage",
+        ignore_paths: ["test/"]
+      ],
       dialyzer: [
         plt_add_apps: [:mix, :ex_unit],
         check_plt: true
@@ -32,6 +37,7 @@ defmodule MediasoupElixir.MixProject do
       # Because nif symlink is not created at first compile: https://github.com/oviceinc/mediasoup-elixir/issues/10
       {:rustler, "~> 0.22.0"},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:lcov_ex, "~> 0.2", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
