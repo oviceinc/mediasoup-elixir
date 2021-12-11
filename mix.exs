4b0b011545a883a014e898c216df2e8866e27655
satoren <mshiraki@ovice.co>
2021-12-11 10:21:55 +0900

    chore: bump to 0.2.1 (#103)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MediasoupElixir.MixProject do
   def project do
     [
       app: :mediasoup_elixir,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
