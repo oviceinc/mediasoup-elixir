06e5631af95ed90cce20fe41467356ee2c3f0878
AkiraFukushima <h3.poteto@gmail.com>
2022-10-20 23:04:59 +0900

    Bump to version 0.5.0 (#172)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MediasoupElixir.MixProject do
   def project do
     [
       app: :mediasoup_elixir,
-      version: "0.4.4",
+      version: "0.5.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
