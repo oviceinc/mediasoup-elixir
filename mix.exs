410e5d078df644cd7af118d85c7fcb79d1c58098
satoren <mshiraki@ovice.co>
2023-08-28 11:43:08 +0900

    increment version to 0.6.0 (#219)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MediasoupElixir.MixProject do
   def project do
     [
       app: :mediasoup_elixir,
-      version: "0.5.6",
+      version: "0.6.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
