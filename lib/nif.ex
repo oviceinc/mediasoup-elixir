993e78f11d175d3eaffe68f2a8d834505d23bad6
Masataka Shiraki <mshiraki@ovice.co>
2024-01-10 14:21:21 +0900

    enable windows prebuild

--- lib/nif.ex
+++ lib/nif.ex
@@ -15,8 +15,8 @@ defmodule Mediasoup.Nif do
         "arm-unknown-linux-gnueabihf",
         "riscv64gc-unknown-linux-gnu",
         "x86_64-apple-darwin",
-        #    "x86_64-pc-windows-gnu",
-        #    "x86_64-pc-windows-msvc",
+        "x86_64-pc-windows-gnu",
+        "x86_64-pc-windows-msvc",
         "x86_64-unknown-linux-gnu",
         "x86_64-unknown-linux-musl"
       ]
