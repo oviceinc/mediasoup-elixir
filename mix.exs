a76a8784765387c87820e9448a671050147cf4b4
satoren <mshiraki@ovice.co>
2024-04-11 10:23:01 +0900

    Update mediasotup to 3.14.1 (#274)
    The cargo package is not updated, so refer directly to github

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MediasoupElixir.MixProject do
   use Mix.Project
 
-  @version "0.10.0"
+  @version "0.11.0-rc.0"
   @repo "https://github.com/oviceinc/mediasoup-elixir"
   @description """
   Elixir wrapper for mediasoup
