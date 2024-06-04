ca69d9f70933b8db949b000215baf5f70d1e73e0
AkiraFukushima <h3.poteto@gmail.com>
2024-06-04 18:57:24 +0900

    Bump to version 0.11.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MediasoupElixir.MixProject do
   use Mix.Project
 
-  @version "0.11.0-rc.1"
+  @version "0.11.0"
   @repo "https://github.com/oviceinc/mediasoup-elixir"
   @description """
   Elixir wrapper for mediasoup
