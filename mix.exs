d1c6423b075e6fa5413c1ecb4606855134109357
Masataka Shiraki <mshiraki@ovice.co>
2024-10-18 11:52:37 +0900

    version up

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MediasoupElixir.MixProject do
   use Mix.Project
 
-  @version "0.12.0"
+  @version "0.13.0"
   @repo "https://github.com/oviceinc/mediasoup-elixir"
   @description """
   Elixir wrapper for mediasoup
